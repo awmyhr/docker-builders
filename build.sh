38004d5bc15b34d273ff5e4dfb6ee5dac52a65f9
awmyhr <awmyhr@gmail.com>
2018-01-01 18:56:02 -0500

    build.sh no has switches to skip flavors and simplified vars

--- build.sh
+++ build.sh
@@ -2,27 +2,31 @@
 #===============================================================================
 # Project:   docker-builders
 # Author:    Myhr, Andy
-# Revised:   2017-12-28
+# Revised:   2018-01-01
 # Created:   2017-12-28
 # Copyright: 2017, awmyhr
 # License:   Apache-2.0
 #===============================================================================
-PROJECT='builders'
-USER='awmyhr'
-
-BASE_NAME="${USER}/${PROJECT}"
+PROJECT='awmyhr/builders'
 
 #-------------------------------------------------------------------------------
 #-- Ubuntu Build Container
 #-------------------------------------------------------------------------------
-printf 'Building %s...\n' "${BASE_NAME}:ubuntu"
+if [ "${*#*--noubuntu}" != "${*}" ]; then
+    printf 'Skipping Ubuntu.\n'
+else
+    printf 'Building %s...\n' "${PROJECT}:ubuntu"
 
-docker build -t "${BASE_NAME}:ubuntu" . -f Dockerfile.ubuntu
+    docker build -t "${PROJECT}:ubuntu" . -f Dockerfile.ubuntu
+fi
 
 #-------------------------------------------------------------------------------
 #-- Alpine Build Container
 #-------------------------------------------------------------------------------
-printf 'Building %s...\n' "${BASE_NAME}:alpine"
+if [ "${*#*--noalpine}" != "${*}" ]; then
+    printf 'Skipping Alpine.\n'
+else
+    printf 'Building %s...\n' "${PROJECT}:alpine"
 
-docker build -t "${BASE_NAME}:alpine" . -f Dockerfile.alpine
-
+    docker build -t "${PROJECT}:alpine" . -f Dockerfile.alpine
+fi
